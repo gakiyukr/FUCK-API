6a4b9b9f750469156485d84a913658b480b00867
avalonsky <Ansetheisia@outlook.kr>
2022-08-13 23:18:29 +0800

    change file

--- fuck-lite/functions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandomNumber = void 0;
-const getRandomNumber = (_max) => {
-    return Math.floor(Math.random() * (_max)) + 0;
-};
-exports.getRandomNumber = getRandomNumber;
